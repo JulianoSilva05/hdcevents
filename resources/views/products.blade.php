bb2b4aac3daf18a1c1c5caeb8c706aec65072ec2
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2021-12-18 00:06:11 -0300

    Incluir Menus

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,5 +3,8 @@
 @section('title', 'Produtos')
 
 @section('content')
-<h1>Tela de produtos</h1>
+    <h1>Tela de produtos</h1>
+    @if($busca != '')
+    <p>O usúario está buscando por : {{$busca}}</p>
+    @endif
 @endsection
