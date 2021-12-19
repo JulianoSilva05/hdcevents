2798717cb4db6bbedc5799b292e28dbdbcf4432a
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2021-12-18 21:51:24 -0300

    Conexão com BD e exclusão de tabelas - #06
    Exclusão das tabelas:
    -products
    
    Inclusão da tabela:
    -events
    
    Alteração da pagina inicial, retirando todo o teto e retornando somente eventos cadastrados no BD.

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,7 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
-Route::get('/events/cadastro', [EventController::class, 'cadastro']);
-Route::get('/events/login', [EventController::class, 'login']);
 
 Route::get('/contact', function () {
     return view('contact');
-});
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('product',['id' => $id]);
 });
