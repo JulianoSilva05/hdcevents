6f2d803d2654a5bb4953907791b33680c1a400bc
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2021-12-19 13:17:00 -0300

    Flash Messages
    Criar Flash Messages

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -38,9 +38,7 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
 
     }
-
-
 }
