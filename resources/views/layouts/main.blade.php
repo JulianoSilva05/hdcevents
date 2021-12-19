1d99f4983880cac30e877ae362e785988fb53e78
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2021-12-19 20:32:06 -0300

    Criar sistema de login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 
-        <!-- CSS da aplicação -->    
+        <!-- CSS da aplicação -->
 
         <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
@@ -35,13 +35,31 @@
                         <li class="nav-item">
                             <a href="/events/create" class="nav-link">Criar Eventos</a>
                         </li>
+                        @auth
                         <li class="nav-item">
-                            <a href="/events/login" class="nav-link">Entrar</a>
+                            <a href="/dashboard" class="nav-link">Meus eventos</a>
                         </li>
                         <li class="nav-item">
-                            <a href="/events/cadastro" class="nav-link">Cadastrar</a>
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <a href="/logout"
+                                class="nav-link"
+                                onclick="event.preventDefault();
+                                this.closest('form').submit();">
+                                Sair
+                                </a>
+                            </form>
+                        </li>
+                        @endauth
+                        @guest
+                        <li class="nav-item">
+                            <a href="/login" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/register" class="nav-link">Cadastrar</a>
                         </li>
 
+                        @endguest
                     </ul>
                 </div>
 
@@ -59,7 +77,7 @@
         </main>
         <footer>
             <p>HDC Events &copy; 2020</p>
-        </footer>   
+        </footer>
         <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     </body>
